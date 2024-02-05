e6cf81b2392906cf271ab705a220e0dfaa8e23c3
Manuhin <manuhin.palath@ccs-technologies.com>
2024-02-05 16:06:22 +0530

    send request

---
+++ src/webparts/sendRequest/components/ISendRequestProps.ts
@@ -0,0 +1,7 @@
+export interface ISendRequestProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/sendRequest/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISendRequestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SendRequestWebPartStrings' {
+  const strings: ISendRequestWebPartStrings;
+  export = strings;
+}
