19480b4b1faf827925375647bae7500efee0036b
Manuhin <manuhin.palath@ccs-technologies.com>
2024-02-05 14:09:01 +0530

    edit document

---
+++ src/webparts/editDocument/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEditDocumentWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EditDocumentWebPartStrings' {
+  const strings: IEditDocumentWebPartStrings;
+  export = strings;
+}
